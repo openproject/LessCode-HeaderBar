43ba94fbc228d935fbc54aa4b7bd01f3a26a8097
fengjian <fengjian@lez.cn>
2017-02-23 17:56:46 +0800

    init project.

---
+++ lesscode-headerbar/src/main/java/com/jayfeng/lesscode/headerbar/HeaderBar.java
@@ -0,0 +1,27 @@
+package com.jayfeng.lesscode.headerbar;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class HeaderBar extends RelativeLayout {
+
+    public HeaderBar(Context context) {
+        super(context);
+    }
+
+    public HeaderBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HeaderBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
